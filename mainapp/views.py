934c35a730ee1066d5e5b941c9311de7c54f758b
Farah Aprilita Rahma <aprilitarahmafarah@gmail.com>
2021-08-10 12:32:45 +0700

    add new page function

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,18 +3,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def landing_page(request):
     return HttpResponse("Hello, Farah Cantik")
-    
+
+
 def second_page(request):
     return HttpResponse("SecondPage")
 
-def count(request,angka):
+
+def count(request, angka):
     angka = angka + 1
     return HttpResponse(str(angka))
 
-def sapa(request,nama):
+
+def sapa(request, nama):
     return HttpResponse("Hello," + nama)
+
 
 def example(request):
     return render(request, "example.html")
+
+
+def newpage(request):
+    return HttpResponse("new")
